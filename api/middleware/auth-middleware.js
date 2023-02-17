045ec9a4e650d2c8c33d0e64b660bdf713ff18d7
Xavier Buckner <xavier@Xaviers-MacBook-Pro.local>
2023-02-17 17:25:14 -0600

    testing

--- api/middleware/auth-middleware.js
+++ api/middleware/auth-middleware.js
@@ -43,8 +43,18 @@ const checkUsernameExists = (req, res, next) => {
     if(!req.body.username || !req.body.password || req.body.username.trim() === '' || req.body.password.trim() === ''){
         res.status(404).json("username and password required")
     }else{
-        next()
-    }
+        User.findByName(req.body.username)
+        .then(users => {
+            if(users.length > 0){
+                req.user = users[0]
+                console.log(req.user)
+                next()
+            }else{
+                res.status(406).json("invalid credentials")
+            }
+        } )
+        }
+}
 
     // const username = req.body.username
 
@@ -60,7 +70,7 @@ const checkUsernameExists = (req, res, next) => {
     //     }).catch(err => {
     //         next(res.status(500).json(err))
     //     })
-}
+
 
 
 
